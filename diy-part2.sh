933d87b545997850da4b5cfbb26cd85fe2a26e74
Atlan-W <128918149+Atlan-W@users.noreply.github.com>
2023-07-29 21:20:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@
 #
 
 # Modify default IP
-sed -i 's/10.24.32.1/10.24.50.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.24.32.1/g' package/base-files/files/bin/config_generate
